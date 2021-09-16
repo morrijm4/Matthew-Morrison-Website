cf2196e5737e8185a59efea2b07a1e3037ef8ce1
Matthew Morrison <james.m.morrison@vanderbilt.edu>
2021-09-16 18:49:35 -0500

    update bio

--- src/mock/data.js
+++ src/mock/data.js
@@ -20,11 +20,12 @@ export const heroData = {
 export const aboutData = {
   img: "profile.JPG",
   paragraphOne:
-    "I am a junior at Vanderbilt University majoring in computer science with a minor in engineering management.",
+    "I am a computer science major at Vanderbilt University graduating in May 2022. ",
   paragraphTwo:
-    "Over the summer, I taught kids ages 8-18 fundamental programming concepts and problem-solving skills through a remote tutoring company called Juni Learning. ",
+    "Over the summer, taught myself how to create websites using JavaScript libraies." +
+    "My preferences are developing with React on the frontend and using Express on the backend.",
   paragraphThree:
-    "In my free time I love to play golf, jam out on my guitar, and go on long distance runs.",
+    "In my free time I enjoy playing golf and jamming out on my guitar",
   paragraphFour: "Programming Languages: C++, Python, Java, JavaSript/HTML/CSS",
   resume:
     "https://drive.google.com/file/d/1rt9CS0Qc-TekFXaSSX85dUjjb1xUvC_k/view?usp=sharing", // if no resume, the button will not show up
@@ -36,8 +37,7 @@ export const projectsData = [
     id: nanoid(),
     img: "spotify-rest-api.png",
     title: "Spotify Rest Api",
-    info:
-      "This project I played around with the spotify rest api and created a backend server with a raspberry pi",
+    info: "This project I played around with the spotify rest api and created a backend server hosted by Digital Ocean",
     info2: "",
     url: "https://matthewmorrison-portfolio.com/SpotifyRestApi", //'https://matthewmorrison-portfolio.com/SpotifyRestApi',
     repo: "https://github.com/morrijm4/spotify-rest-api", // if no repo, the button will not show up
@@ -46,8 +46,7 @@ export const projectsData = [
     id: nanoid(),
     img: "calculator.png",
     title: "Calculator",
-    info:
-      "This was my first project I have build from scratch using HTML, CSS, and JavaScript. It taught me the basics of how to design a website and how to make it dynamic with JavaScript.",
+    info: "This was my first project I have build from scratch using HTML, CSS, and JavaScript. It taught me the basics of how to design a website and how to make it dynamic with JavaScript.",
     info2: "",
     url: "https://fervent-yalow-77b5c7.netlify.app/",
     repo: "https://github.com/morrijm4/Calculator", // if no repo, the button will not show up
@@ -56,8 +55,7 @@ export const projectsData = [
     id: nanoid(),
     img: "mini-game.png",
     title: "Tweet Gusser Mini-Game",
-    info:
-      "This is a game where you have to guess whether a tweet was tweeted by Elon Musk or Kanye West.",
+    info: "This is a game where you have to guess whether a tweet was tweeted by Elon Musk or Kanye West.",
     info2:
       "It was written 100% in Python using the tweepy library to access the Twitter API. This project was a coding challenge to join Change++, a student run organization that designs and builds applications for non-profit organizations.",
     url: "",
@@ -67,8 +65,7 @@ export const projectsData = [
     id: nanoid(),
     img: "sudoku-solver.png",
     title: "Sudoku Solver",
-    info:
-      "Recursive backtracking algorithm that can solve a sudoku puzzle with an average time of 0.001567 seconds. This program was written in C++.",
+    info: "Recursive backtracking algorithm that can solve a sudoku puzzle with an average time of 0.001567 seconds. This program was written in C++.",
     info2: "",
     url: "",
     repo: "https://github.com/morrijm4/Sudoku-Solver", // if no repo, the button will not show up
